26acfd155ac4a0ef6df650754f4a7588f3426bb6
CodeMyst <codemyst@outlook.com>
2018-10-15 18:28:22 +0200

    Removed trim option

--- screenshot.sh
+++ screenshot.sh
@@ -2,6 +2,6 @@ screenshotLocation=~/Files/Images/Screenshots/
 
 date=$(date +%Y-%m-%d-%H-%M-%S)
 filename='screenshot.'$date'.png'
-import -trim $filename
+import $filename
 mv --backup=numbered $filename $screenshotLocation
 xclip -selection clipboard -t image/png -i $screenshotLocation$filename
